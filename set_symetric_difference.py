8c3ed7926a0bd75f72d70af8e50345b4a1e88aaa
Z-code957 <pallavisethupadhyay@gmail.com>
2025-10-08 17:10:59 +0800

    Add files via upload

---
+++ set_symetric_difference.py
@@ -0,0 +1,8 @@
+set1 = {"blue","green"}
+set2 = {"blue","yellow"}
+setA = {1,2,3,4,5}
+setB = {1,5,6,7,8,9}
+difference = set1.symmetric_difference(set2)
+difference2 = setA.symmetric_difference(setB)
+print(difference)
+print(difference2)
